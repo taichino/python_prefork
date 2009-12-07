d99889bf1fe9766b0c33b16540b9bf2a79e994ee
taichino <taichino@gmail.com>
2009-12-07 15:22:56 +0900

    add module explanation to __init__.py

--- python_prefork/__init__.py
+++ python_prefork/__init__.py
@@ -1,3 +1,33 @@
+"""
+   This module is inspired by Parallel::Prefork in CPAN.
+   As Parallel::Prefork, this module is intended to be
+   some operations done in parallel.
+
+   Simple example of usage is followings
+
+       >>> from python_prefork import PythonPrefork
+       >>> pp = PythonPrefork()
+       >>> while not pp.signal_received:
+       >>>     if pp.start(): continue
+       >>>
+       >>>     run() # do some task in child process
+       >>> 
+       >>>     pp.finish()
+       >>> pp.wait_all_children()
+
+   Some options can be set in constructor
+       [ max_workers ]
+       maximum number of child processes to fork
+       
+       [trap_signals]
+       arrays of signals to be trapped.
+       parent process will send these signals to all children
+
+       [on_reap_cb]
+       function to be called when a child is end.
+       This function must have two parameters, child pid and exit status.
+"""
+
 __author__  = "Matsumoto Taichi (taichino@gmail.com)"
 __version__ = "0.1"
 __license__ = "MIT License"
