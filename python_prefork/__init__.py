4565523d48c32d22444cff2cf45e5d09546c375a
taichino <taichino@gmail.com>
2009-12-13 21:40:32 +0900

    modify package configuration

--- python_prefork/__init__.py
+++ python_prefork/__init__.py
@@ -29,5 +29,7 @@
 """
 
 __author__  = "Matsumoto Taichi (taichino@gmail.com)"
-__version__ = "0.1"
+__version__ = "0.1.1"
 __license__ = "MIT License"
+
+from python_prefork import PythonPrefork
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 import unittest
 
 import python_prefork
-from python_prefork import __version__
+from python_prefork import __version__, __license__, __author__
 from python_prefork import python_prefork_test
 
 if __name__ == '__main__':
@@ -25,11 +25,12 @@ if __name__ == '__main__':
     py_modules       = ['python_prefork', 'python_prefork_test'],
     description      = 'parallel processing fork manager inspired by Parallel::Prefork in CPAN',
     long_description = python_prefork.__doc__,
-    author           = 'Matsumoto Taichi',
+    author           = __author__,
     author_email     = 'taichino@gmail.com',
     url              = 'http://github.com/taichino/python_prefork',
     keywords         = 'multi process, parallel, prefork',
-    license          = 'MIT',
+    license          = __license__,
+    packages         = ('python_prefork',),
     classifiers      = ["Development Status :: 3 - Alpha",
                         "Intended Audience :: Developers",
                         "License :: OSI Approved :: MIT License",
